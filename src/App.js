f62fda27b9683c903ba778054f45713922cc6f45
Colin McNamara <CEG118@Robyns-Macintosh.local>
2017-02-02 11:27:44 -0500

    test apostrophe

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>React ToDo's</h2>
+          <h2>React ToDos</h2>
         </div>
 
         <div className="Todo-App">
